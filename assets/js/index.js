2ec4a106a05e46bfa3ca4e44f7e6796fc4ddfa43
Igor Kroshkin <igor.krosh@gmail.com>
2020-12-03 00:20:43 +0300

    Валидация форм

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,8 +26,32 @@ function SetForm()
         $('#modalContactForm').modal('show')
     });
 
-    $('.btn-send-form').on('click', function () {
-        $('.modal').modal('hide');
-        $('#sendModal').modal('show');
-    });
+    $.validator.addMethod('checkMask', function(value, element) {
+        return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value); 
+    })
+
+    let validateSetting = {
+        rules: {
+            phone: {
+                checkMask: true
+            }
+        },
+        messages: {
+            phone: {
+                checkMask: "Введите полный номер телефона"
+            }
+        },
+        submitHandler: SubmitForm
+    }
+
+    $('.contact-us form').validate(validateSetting);
+    $('.modal form').validate(validateSetting);
+
+    $('form input[name=phone]').mask("+7(999)999-9999", {autoclear: false});
+}
+
+function SubmitForm()
+{
+    $('.modal').modal('hide');
+    $('#sendModal').modal('show');
 }
